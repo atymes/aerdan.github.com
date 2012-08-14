41fc9cf16f986b53e5cf90660fd793f43b30d02d
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2012-08-14 14:52:09 -0400

    Roll in features from Inari.

---
+++ js/site.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    if ($.cookie('text-shadow') == 'no') {
+        $('body').css('text-shadow', 'none');
+        $('#no-shadow').attr('checked', 'checked');
+    }
+    $('#no-shadow').click(function(){
+        if ($.cookie('text-shadow') == 'no') {
+            $.cookie('text-shadow', 'yes');
+        } else {
+            $.cookie('text-shadow', 'no');
+        }
+    });
+    $('#style').change(function() {
+        $.cookie('stylesheet', $('#style').val());
+    });
+    if (!$.cookie('stylesheet')) {
+        $.cookie('stylesheet', 'ff4');
+    } else {
+        $('#style').val($.cookie('stylesheet'));
+    }
+    var style = $.cookie('stylesheet');
+    if (style != 'ff4') {
+        $("head").append('<li-nk href="/css/' + style + '.css" rel="stylesheet">');
+    }
+});
+
